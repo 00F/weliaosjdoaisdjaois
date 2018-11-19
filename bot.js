4414015fa7da83b2c820247aeaa64931990f14ff
` 0ff <39999036+00F@users.noreply.github.com>
2018-11-19 13:07:19 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js'),
+    meercy = new Discord.Client({sisableEveryone: true})
+    console.log("muuuuuuuuute,");
+meeercy.on('guildMemberAdd', member => {
+const fuli= member.guild.channels.get("481277680109486110");
+if(!fuli) return;
+if(fuli) {
+setTimeout(() => fuli.send(Welcome To **NoThing.**), 4000)
+}
+});
+meercy.login(process.env.MEERCY);
