54706cd1f9ff19cde9bf9d459da07ea3644a9c22
` 0ff <39999036+00F@users.noreply.github.com>
2018-11-19 13:14:55 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js'),
     meercy = new Discord.Client({sisableEveryone: true})
     console.log("muuuuuuuuute,");
-meeercy.on('guildMemberAdd', member => {
+meercy.on('guildMemberAdd', member => {
 const fuli= member.guild.channels.get("481277680109486110");
 if(!fuli) return;
 if(fuli) {
